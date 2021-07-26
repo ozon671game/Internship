fc70131021b24b6061bd9854819fe51f257a8411
ozone671game <sapog.strausa@mail.ru>
2021-07-27 00:50:59 +0300

    // array[1..100], stream, [2,4, .... 8*7

--- threads.dart
+++ threads.dart
@@ -3,7 +3,28 @@ import 'dart:core';
 
 void main(){
 
-  var counterStream = Stream<int>.periodic(const Duration(seconds: 1), (int x) {return x;});
-  var doubleCounterStream = counterStream.map((int x) => x * 2);
-  doubleCounterStream.forEach(print);
+  // array[1..100], stream, [2,4, .... 8*7
+  var stream = create();
+  // edit(stream);
+  // stream.forEach(print);
+  var stream1 = edit(stream);
+  stream1.forEach(print);
+}
+
+Stream<int> create() async*{
+  for(var i = 1; i <= 100; i++) {
+    yield i;
+  }
+}
+
+Stream<int> edit(Stream<int> stream) async*{
+  await for(var e in stream){
+    if(e % 2 == 0){
+      if(e % 8 == 0){
+        yield (e*7);
+        continue;
+      }
+      yield e;
+    }
+  }
 }
